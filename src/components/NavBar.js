b2e0720f997a8f9b7c612efd23136f6019c7d2fa
Christina Greenwood <greenwoodc@me.com>
2022-11-15 13:43:16 -0700

    added feature for navbar, underline when tab is selected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+  let activeStyle = {
+    color: "black",
+    borderBottom: "1px solid black",
+  };
+
+  let activeClassName = "underline";
+
   return (
     <div className='nav-wrapper'>
       <div className='left-side'>
         <nav>
           <div className='nav-link-wrapper'>
-            <NavLink to='/' className='nav-link-active'>
+            <NavLink
+              to='/'
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
               Home
             </NavLink>
           </div>
           <div className='nav-link-wrapper'>
-            <NavLink to='/about' className='nav-link-active'>
+            <NavLink
+              to='/about'
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
               About
             </NavLink>
           </div>
           <div className='nav-link-wrapper'>
-            <NavLink to='/contact' className='nav-link-active'>
+            <NavLink
+              to='/contact'
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
               Contact
             </NavLink>
           </div>
           <div className='nav-link-wrapper'>
-            <NavLink to='/resume' className='nav-link-active'>
+            <NavLink
+              to='/resume'
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
               Resume
             </NavLink>
           </div>
